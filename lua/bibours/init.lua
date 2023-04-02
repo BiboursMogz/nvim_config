37415311b40b19d5f6c1611b340ea9b48142dead
BiboursMogz <antmaugas+github@gmail.com>
2023-04-02 16:17:28 +0200

    Chore: moves leader remap first

--- lua/bibours/init.lua
+++ lua/bibours/init.lua
@@ -1,2 +1,8 @@
+-- Set <space> as the leader key
+-- See `:help mapleader`
+--  NOTE: Must happen before plugins are required (otherwise wrong leader will be used)
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
 require("bibours.packer")
 require("bibours.remap")
